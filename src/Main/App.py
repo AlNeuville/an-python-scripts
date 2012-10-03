1438eed0095358623db09c9c03c36f6f05a075f2
Alexandre <spirit.knight666@gmail.com>
2012-10-03 13:45:03 +0200

    Initial commit

---
+++ src/Main/App.py
@@ -0,0 +1,42 @@
+#!usr/bin/env python
+# -*- coding:utf-8 -*-
+
+'''\
+Created on 21 juin 2012
+@author: Alexandre Neuville
+@version: 0.0.1-SNAPSHOT
+
+This program manages a list of scripts. It can execute the scripts and show
+their outputs.
+Options:
+    -h, --help:    Display this help message.
+    -f, --cfg:     The configuration file.\
+'''
+
+from wx import App
+from json import load
+from getopt import getopt
+from sys import argv, exit
+from Controller import Controller
+
+if __name__ == "__main__":
+
+    options = getopt(argv[1:], "hf:", ["help", "cfg="])
+    for option, value in options[0]:
+        if option == "-h" or option == "--help":
+            print __doc__
+            exit(0)
+        elif option == "-f" or option == "--cfg":
+            configFileName = value
+
+    parameters = None
+    with open(configFileName) as f:
+        parameters = load(f, f.encoding)
+    if not parameters:
+        print "Configuration not read"
+        exit(1)
+
+    app = App(False)
+    app.SetVendorName("Alexandre Neuville")
+    controller = Controller(**parameters)
+    app.MainLoop()
