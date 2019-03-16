c03067db1eb297e2c13ca72c4ffbf4a19777c68b
AlNeuville <11499521+AlNeuville@users.noreply.github.com>
2019-03-16 17:10:46 +0100

    final setup file

--- setup.py
+++ setup.py
@@ -3,10 +3,12 @@ from setuptools import setup
 setup(
 	name='an-python-scripts',
 	version='0.1.0',
+	scripts=['scripts/app'],
 	packages=['app'],
 	url='',
 	license='license.txt',
 	author='Sorondare',
 	author_email='',
-	description='Program to expose output of a list of console commands'
+	description='Program to expose output of a list of console commands',
+	data_files=[('config', ['resources/config.ini'])]
 )
