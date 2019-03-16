0c7540ae8a1200a4ad793950282505545969f995
AlNeuville <11499521+AlNeuville@users.noreply.github.com>
2019-03-16 16:30:37 +0100

    repackage

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+	name='an-python-scripts',
+	version='0.1.0',
+	packages=[''],
+	package_dir={'': 'src/main'},
+	url='',
+	license='MIT',
+	author='Sorondare',
+	author_email='',
+	description='Program to manage output of console command'
+)
--- src/main/resources/scripts/test2.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-setlocal
-
-set MOI=jours de l'annee
-echo %1 %MOI% avant Noel
-ping localhost -n 4
-
-endlocal 
